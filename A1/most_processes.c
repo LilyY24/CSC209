23ff55d252d65ea7a46b2fc18291c298723cbfa7
will <will.qie@mail.utoronto.ca>
2018-09-16 12:40:56 -0400

    finish a part

--- A1/most_processes.c
+++ A1/most_processes.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-char* handle_without_argument(char** content);
+int handle_without_argument(char** content, int length);
 
-char* handle_with_argument(char** content, int pid);
+int handle_with_argument(char** content, int pid, int length);
+
+char* get_name(char* line);
+
+int get_pid(char* line);
 
 int main(int arg, char **argv){
     if (arg >= 3){
@@ -26,20 +31,49 @@ int main(int arg, char **argv){
         }
     }
 
-    // Handele the input accordingly
-    // if (arg == 1){
-    //     printf("%s", handle_without_argument(content));
-    // } else{
-    //     printf("%s", handle_with_argument(content, (int)argv[1]));
-    // }
+    //Handle the input accordingly
+    if (arg == 1){
+        handle_without_argument(content, used);
+    } else{
+        handle_with_argument(content, (int)argv[1], used);
+    }
     return 0;
 }
 
-char* handle_without_argument(char** content){
-    return "a";
+int handle_without_argument(char** content, int length){
+    char** names = (char**)malloc(sizeof(char*) * length);
+    int numbers[length];
+    int cursor = 0;
+    int num_people = 0;
+    // init numbers to 0;
+    for (int i = 0; i < length; i++){
+        numbers[i] = 0;
+    }
+    for (int i = 0; i < length; i++){
+        if (i == 0){
+            names[0] = get_name(content[0]);
+            numbers[0]++;
+            num_people++;
+        } else if (strcmp(get_name(content[i-1]), get_name(content[i]))){
+            numbers[cursor]++;
+        } else{
+            cursor++;
+            num_people++;
+            names[cursor] = get_name(content[i]);
+            numbers[cursor]++;
+        }
+    }
+    int winner_cursor = 0;
+    for (int i =0; i < num_people; i++){
+        if (numbers[i] > numbers[winner_cursor]){
+            winner_cursor = i;
+        }
+    }
+    printf("%s %d", *names[winner_cursor], numbers[winner_cursor]);
+    return 0;
 }
 
-char* handle_with_argument(char** content, int pid){
+int handle_with_argument(char** content, int pid, int length){
     pid++;
     return "a";
 }
