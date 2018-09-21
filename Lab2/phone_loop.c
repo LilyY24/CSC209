44484c5f25575ec9d91c4e3dfe97e4cae40ed9e6
will <will.qie@mail.utoronto.ca>
2018-09-21 08:25:39 -0400

    Slight modify

--- Lab2/phone_loop.c
+++ Lab2/phone_loop.c
@@ -4,21 +4,16 @@
 int main(){
     char phone[11];
     int num;
-    int has_error = 0;
     scanf("%s", phone);
     while(scanf("%d", &num) != EOF){
         if (num < -1 || num > 9){
             printf("ERROR\n");
-            has_error = 1;
         }
         if (num == -1){
             printf("%s\n", phone);
         } else{
             printf("%c\n", phone[num]);
         }
-    }
-    if (has_error){
-        return 1;
     }
     return 0;
 }
